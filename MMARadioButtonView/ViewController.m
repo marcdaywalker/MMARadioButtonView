e378534af9d86d1efd34ddfe9e74fbbe84af9730
marcdaywalker <marcdaywalker@gmail.com>
2015-05-27 11:22:35 +0100

    add more examples

--- MMARadioButtonView/ViewController.m
+++ MMARadioButtonView/ViewController.m
@@ -19,28 +19,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    MMARadioButtonView *radioButtonView = [[MMARadioButtonView alloc] radioButtonWithFrame:CGRectMake(kPadding, kPadding, CGRectGetWidth(self.view.frame)-kPadding*2,50)];
-    radioButtonView.delegate = self;
-    [radioButtonView.titleLabel setText:@"Distance"];
-    radioButtonView.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     
-    MMARadioButton *radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(120, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
-    [radioButton.textLabel setText:@"Km"];
+    MMARadioButtonView *radioButtonView = [[MMARadioButtonView alloc] radioButtonWithFrame:CGRectMake(0, kPadding, CGRectGetWidth(self.view.frame),50)];
+    radioButtonView.delegate = self;
+    [radioButtonView.titleLabel setText:@"Two options"];
+    radioButtonView.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    [self.view addSubview:radioButtonView];
+    
+    MMARadioButton *radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(130, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
+    [radioButton.textLabel setText:@"Yes"];
     radioButton.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-//    radioButton.textLabel.textAlignment = NSTextAlignmentLeft;
-//    radioButton.textLabel.frame = CGRectMake(0, 0, CGRectGetWidth(radioButton.frame), CGRectGetHeight(radioButton.frame));
     radioButton.check = NO;
     [radioButtonView addRadioButton:radioButton];
     
-    radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(200, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
-    [radioButton.textLabel setText:@"Mi"];
+    radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(210, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
+    [radioButton.textLabel setText:@"No"];
     radioButton.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-//    radioButton.textLabel.textAlignment = NSTextAlignmentLeft;
-//    radioButton.textLabel.frame = CGRectMake(0, 0, CGRectGetWidth(radioButton.frame), CGRectGetHeight(radioButton.frame));
     radioButton.check = YES;
     [radioButtonView addRadioButton:radioButton];
+    
+    
+    radioButtonView = [[MMARadioButtonView alloc] radioButtonWithFrame:CGRectMake(0, CGRectGetMaxY(radioButtonView.frame) + kPadding, CGRectGetWidth(self.view.frame),50)];
+    radioButtonView.delegate = self;
+    [radioButtonView.titleLabel setText:@"Three options"];
+    radioButtonView.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     [self.view addSubview:radioButtonView];
+    
+    radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(130, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
+    [radioButton.textLabel setText:@"A"];
+    radioButton.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    radioButton.check = YES;
+    [radioButtonView addRadioButton:radioButton];
+    
+    radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(210, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
+    [radioButton.textLabel setText:@"B"];
+    radioButton.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    radioButton.check = NO;
+    [radioButtonView addRadioButton:radioButton];
+    
+    radioButton = [[MMARadioButton alloc] initMMARadioButtonWithFrame:CGRectMake(290, 0, CGRectGetHeight(radioButtonView.frame), CGRectGetHeight(radioButtonView.frame))];
+    [radioButton.textLabel setText:@"C"];
+    radioButton.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    radioButton.check = NO;
+    [radioButtonView addRadioButton:radioButton];
+    
+}
+
+#pragma mark - MMARadioButtonViewDelegate
+- (void)selectedRadioButtonView:(MMARadioButtonView *)radioButtonView usingRadioButton:(MMARadioButton *)radioButton
+{
+    NSLog(@"RadioButtonView title: %@ radioButton: %@",radioButtonView.titleLabel.text, radioButton.textLabel.text);
 }
 
 - (void)didReceiveMemoryWarning {
